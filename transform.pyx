e83a7a9409f0d1b7e748a67dc77deea74d30e0e7
Log1cG8z <yyukki2009@gmail.com>
2025-03-31 13:00:59 +0900

    AVX-512に改良

--- transform.pyx
+++ transform.pyx
@@ -4,64 +4,50 @@ cimport cython
 cimport numpy as cnp
 cdef extern from *:
 	"""
-	#include <xmmintrin.h>
-	#include <emmintrin.h>
-    #include <tmmintrin.h>
+	#include <immintrin.h>
 	"""
-	ctypedef struct __m128d: pass # 64ビット浮動小数点数 x 2
-	ctypedef struct __m128i: pass # 32ビット整数 x 4
-	ctypedef struct __m128:	pass # 32ビット浮動小数点数 x 4
-	__m128i _mm_set_epi16(short e7, short e6, short e5, short e4, short e3, short e2, short e1, short e0)
-	__m128i _mm_set_epi32(int e3, int e2, int e1, int e0)
-	__m128i _mm_shuffle_epi32(__m128i a, int imm8)
-	__m128i _mm_add_epi32(__m128i a, __m128i b)
-	__m128i _mm_hadd_epi32(__m128i a, __m128i b)
-	__m128i _mm_sub_epi32(__m128i a, __m128i b)
-	__m128i _mm_hsub_epi32(__m128i a, __m128i b)
-	__m128i _mm_castpd_si128(__m128d a)
-	__m128d _mm_move_sd (__m128d a, __m128d b)
-	__m128d _mm_castsi128_pd(__m128i a)
-	__m128 _mm_cvtepi32_ps(__m128i a)
-	void _mm_store_si128(__m128i* p, __m128i a)
-	__m128 _mm_store_ps(float *p, __m128 a)
+	ctypedef struct __m512i: pass
+	ctypedef struct __m512h: pass
+	ctypedef struct __mmask8: pass
+	__m512i _mm512_set_epi16(short e31, short e30, short e29, short e28, short e27, short e26, short e25, short e24, short e23, short e22, short e21, short e20, short e19, short e18, short e17, short e16, short e15, short e14, short e13, short e12, short e11, short e10, short e9, short e8, short e7, short e6, short e5, short e4, short e3, short e2, short e1, short e0)
+	__m512i _mm512_mask_mov_epi64(__m512i src, __mmask8 k, __m512i a)
+	__m512h _mm512_cvtepu16_ph(__m512i a)
+	__m512i _mm512_castph_si512(__m512h a)
+	__m512h _mm512_castsi512_ph(__m512i a)
+	__m512h _mm512_add_ph(__m512h a, __m512h b)
+	__m512h _mm512_sub_ph(__m512h a, __m512h b)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def dct_time_fwd(cnp.ndarray[cnp.uint16_t, ndim = 1] a):
-	cdef unsigned short s = 0
-    # convert uint16 to int32
-	cdef __m128i n0 = _mm_set_epi16(s, a[3], s, a[2], s, a[1], s, a[0])
-	cdef __m128i n1 = _mm_set_epi16(s, a[7], s, a[6], s, a[5], s, a[4])
-    # stage 1
-	cdef __m128i n2 = _mm_add_epi32(n0, n1)
-	cdef __m128i n3 = _mm_sub_epi32(n0, n1)
-    # stage 2
-	cdef __m128i n4 = _mm_hadd_epi32(n2, n3)
-	cdef __m128i n5 = _mm_hsub_epi32(n2, n3)
-    # stage 3
-	cdef __m128i n6 = _mm_hadd_epi32(n4, n5)
-	cdef __m128i n7 = _mm_hsub_epi32(n4, n5)
-    # ordering
-	cdef __m128i n8 = _mm_shuffle_epi32(n6, 141)
-	cdef __m128i n9 = _mm_shuffle_epi32(n7, 216)
-	# casting to inter-register ordering
-	cdef __m128d n10 = _mm_castsi128_pd(n8)
-	cdef __m128d n11 = _mm_castsi128_pd(n9)
-	# inter-register ordering
-	cdef __m128d n12 = _mm_move_sd(n10, n11)
-	cdef __m128d n13 = _mm_move_sd(n11, n10)
-	# casting to integer
-	cdef __m128i n14 = _mm_castpd_si128(n12)
-	cdef __m128i n15 = _mm_castpd_si128(n13)
-	# re-ordering
-	cdef __m128i n16 = _mm_shuffle_epi32(n14, 78)
-	cdef cnp.ndarray[cnp.int32_t, ndim=1] result = np.empty(8, dtype = np.int32)
-	cdef cnp.ndarray[cnp.int32_t, ndim=1] tmp0 = np.empty(4, dtype = np.int32)
-	cdef cnp.ndarray[cnp.int32_t, ndim=1] tmp1 = np.empty(4, dtype = np.int32)
-	cdef int* tdata0 = <int*> tmp0.data
-	cdef int* tdata1 = <int*> tmp1.data
-	_mm_store_si128(<__m128i*> tdata0, n16)
-	_mm_store_si128(<__m128i*> tdata1, n15)
-	result[:4] = tmp0
-	result[4:] = tmp1
+	cdef __mmask8 m0 = 15
+	cdef __mmask8 m1 = 240
+	cdef __mmask8 m2 = 51
+	cdef __mmask8 m3 = 204
+	# loading
+	cdef __m512i v0 = _mm512_set_epi16(a[31], a[30], a[29], a[28], a[27], a[26], a[25], a[24], a[23], a[22], a[21], a[20], a[19], a[18], a[17], a[16], a[15], a[14], a[13], a[12], a[11], a[10], a[9], a[8], a[7], a[6], a[5], a[4], a[3], a[2], a[1], a[0])
+	cdef __m512i v1 = _mm512_set_epi16(a[63], a[62], a[61], a[60], a[59], a[58], a[57], a[56], a[55], a[54], a[53], a[52], a[51], a[50], a[49], a[48], a[47], a[46], a[45], a[44], a[43], a[42], a[41], a[40], a[39], a[38], a[37], a[36], a[35], a[34], a[33], a[32])
+	cdef __m512h v2 = _mm512_cvtepu16_ph(v0)
+	cdef __m512h v3 = _mm512_cvtepu16_ph(v1)
+	# stage 1
+	cdef __m512h v4 = _mm512_add_ph(v2, v3)
+	cdef __m512h v5 = _mm512_sub_ph(v2, v3)
+	# move
+	cdef __m512i v6 = _mm512_castph_si512(v4)
+	cdef __m512i v7 = _mm512_castph_si512(v5)
+	cdef __m512i v8 = _mm512_mask_mov_epi64(v6, m1, v7)
+	cdef __m512i v9 = _mm512_mask_mov_epi64(v7, m0, v6)
+	cdef __m512h v10 = _mm512_castsi512_ph(v8)
+	cdef __m512h v11 = _mm512_castsi512_ph(v9)
+	# stage 2
+	cdef __m512h v12 = _mm512_add_ph(v10, v11)
+	cdef __m512h v13 = _mm512_sub_ph(v10, v11)
+	# move
+	cdef __m512i v14 = _mm512_castph_si512(v12)
+	cdef __m512i v15 = _mm512_castph_si512(v13)
+	cdef __m512i v16 = _mm512_mask_mov_epi64(v14, m3, v15)
+	cdef __m512i v17 = _mm512_mask_mov_epi64(v15, m2, v14)
+	cdef __m512h v18 = _mm512_castsi512_ph(v16)
+	cdef __m512h v19 = _mm512_castsi512_ph(v17)
+	cdef cnp.ndarray[cnp.uint16_t, ndim = 1] result = np.empty(64, np.uint16)
 	return result
