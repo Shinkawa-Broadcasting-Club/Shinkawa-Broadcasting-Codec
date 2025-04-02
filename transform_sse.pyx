c3d3437837041cb457a53cdf91fb80b73d9171d6
Log1cG8z <yyukki2009@gmail.com>
2025-04-03 04:22:54 +0900

    変換改良
    x = (レジスタbit数)/(最小浮動小数点数のbit数)
    x個/毎レジスタを格納し、変換サイズ(パディング自体は8の倍数、あとはゼロ埋め)を 8x bitとすれば移植性も高そう
    部室に戻ったらAVX-512の方もやりたいところ

--- transform_sse.pyx
+++ transform_sse.pyx
@@ -2,47 +2,138 @@ import numpy as np
 cimport cython
 cimport numpy as cnp
 from libc.string cimport memcpy
+from cython.parallel import prange
 cdef extern from *:
 	"""
-	#include <xmmintrin.h>
-	#include <emmintrin.h>
-    #include <tmmintrin.h>
+    #include <nmmintrin.h>
 	"""
-	ctypedef struct __m128d: pass # 64ビット浮動小数点数 x 2
 	ctypedef struct __m128i: pass # 32ビット整数 x 4
 	ctypedef struct __m128:	pass # 32ビット浮動小数点数 x 4
+	ctypedef struct __m64: pass
+	__m64 _mm_set_pi16(short e3, short e2, short e1, short e0)
+	__m128i _mm_loadu_si64(const void *mem_addr)
+	__m128i _mm_cvtepu16_epi32(__m128i a)
+	__m128i _mm_movpi64_epi64(__m64 a)
 	__m128i _mm_set_epi16(short e7, short e6, short e5, short e4, short e3, short e2, short e1, short e0)
-	__m128i _mm_add_epi32(__m128i a, __m128i b)
-	__m128i _mm_hadd_epi32(__m128i a, __m128i b)
-	__m128i _mm_sub_epi32(__m128i a, __m128i b)
-	__m128i _mm_hsub_epi32(__m128i a, __m128i b)
-	__m128i _mm_castps_si128 (__m128 a)
+	__m128i _mm_set_epi32(int e3, int e2, int e1, int e0)
+	__m128i _mm_slli_epi32(__m128i a, int imm8)
+	__m128i _mm_and_si128(__m128i a, __m128i b)
+	__m128i _mm_castps_si128(__m128 a)
+	__m128 _mm_loadu_ps(const float *mem_addr)
+	void _mm_storeu_ps(float* mem_addr, __m128 a)
+	void _MM_TRANSPOSE4_PS (__m128 row0, __m128 row1, __m128 row2, __m128 row3)
+	__m128 _mm_cvtpu16_ps(__m64 a)
 	__m128 _mm_castsi128_ps(__m128i a)
-	__m128 _mm_shuffle_ps (__m128 a, __m128 b, unsigned int imm8)
+	__m128 _mm_add_ps(__m128 a, __m128 b)
+	__m128 _mm_sub_ps(__m128 a, __m128 b)
+	__m128 _mm_mul_ps(__m128 a, __m128 b)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def dct_time_fwd(cnp.ndarray[cnp.uint16_t, ndim = 1] a):
-	cdef unsigned short s = 0
-	# convert uint16 to int32
-	cdef __m128i n0 = _mm_set_epi16(s, a[3], s, a[2], s, a[1], s, a[0])
-	cdef __m128i n1 = _mm_set_epi16(s, a[7], s, a[6], s, a[5], s, a[4])
+cdef dct_time_fwd(cnp.ndarray[cnp.float32_t, ndim = 1] a):
+	cdef:
+		__m128 n0, n1, n2, n3, n4, n5, n6, n7, v0, v1, v2, v3, v4, v5, v6, v7
+		float* data = &a[0]
+		np.ndarray[np.float32_t, ndim = 1] out = np.empty(32, dtype = np.float32)
+	# load
+	n0 = _mm_loadu_ps(data)
+	n1 = _mm_loadu_ps(data + 4)
+	n2 = _mm_loadu_ps(data + 8)
+	n3 = _mm_loadu_ps(data + 12)
+	n4 = _mm_loadu_ps(data + 16)
+	n5 = _mm_loadu_ps(data + 20)
+	n6 = _mm_loadu_ps(data + 24)
+	n7 = _mm_loadu_ps(data + 28)
+	_MM_TRANSPOSE4_PS(n0, n2, n4, n6)
+	_MM_TRANSPOSE4_PS(n1, n3, n5, n7)
 	# stage 1
-	cdef __m128i n2 = _mm_add_epi32(n0, n1)
-	cdef __m128i n3 = _mm_sub_epi32(n0, n1)
+	v0 = _mm_add_ps(n0, n4)
+	v1 = _mm_add_ps(n1, n5)
+	v2 = _mm_add_ps(n2, n6)
+	v3 = _mm_add_ps(n3, n7)
+	v4 = _mm_sub_ps(n0, n4)
+	v5 = _mm_sub_ps(n1, n5)
+	v6 = _mm_sub_ps(n2, n6)
+	v7 = _mm_sub_ps(n3, n7)
 	# stage 2
-	n0 = _mm_hadd_epi32(n2, n3)
-	n1 = _mm_hsub_epi32(n2, n3)
+	n0 = _mm_add_ps(v0, v2)
+	n1 = _mm_add_ps(v1, v3)
+	n2 = _mm_sub_ps(v0, v2)
+	n3 = _mm_sub_ps(v1, v3)
+	n4 = _mm_add_ps(v4, v6)
+	n5 = _mm_add_ps(v5, v7)
+	n6 = _mm_sub_ps(v4, v6)
+	n7 = _mm_sub_ps(v5, v7)
 	# stage 3
-	n2 = _mm_hadd_epi32(n0, n1)
-	n3 = _mm_hsub_epi32(n0, n1)
-	# ordering[(0, 4, 1, 5, 2, 6, 3, 7) => (0, 1, 2, 3, 4, 5, 6, 7)]
-	cdef __m128 n4 = _mm_castsi128_ps(n2)
-	cdef __m128 n5 = _mm_castsi128_ps(n3)
-	cdef __m128 n6 = _mm_shuffle_ps(n4, n5, 136)
-	cdef __m128 n7 = _mm_shuffle_ps(n4, n5, 221)
-	cdef cnp.ndarray[cnp.int32_t, ndim=1] result = np.empty(8, dtype=np.int32)
-	memcpy(&result[0], &n6, 16)
-	memcpy(&result[4], &n7, 16)
-	return result
+	v0 = _mm_add_ps(n0, n1)
+	v1 = _mm_sub_ps(n0, n1)
+	v2 = _mm_add_ps(n2, n3)
+	v3 = _mm_sub_ps(n2, n3)
+	v4 = _mm_add_ps(n4, n5)
+	v5 = _mm_sub_ps(n4, n5)
+	v6 = _mm_add_ps(n6, n7)
+	v7 = _mm_sub_ps(n6, n7)
+	# set to ndarray
+	_MM_TRANSPOSE4_PS(v0, v2, v4, v6)
+	_MM_TRANSPOSE4_PS(v1, v3, v5, v7)
+	_mm_storeu_ps(&out[0], v0)
+	_mm_storeu_ps(&out[4], v1)
+	_mm_storeu_ps(&out[8], v2)
+	_mm_storeu_ps(&out[12], v3)
+	_mm_storeu_ps(&out[16], v4)
+	_mm_storeu_ps(&out[20], v5)
+	_mm_storeu_ps(&out[24], v6)
+	_mm_storeu_ps(&out[28], v7)
+	return out
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef dct_fwd(cnp.ndarray[cnp.float32_t, ndim = 1] a):
+	cdef:
+		__m128 n0, n1, n2, n3, n4, n5, n6, n7, v0, v1, v2, v3, v4, v5, v6, v7
+		float* data = &a[0]
+		np.ndarray[np.float32_t, ndim = 1] out = np.empty(32, dtype = np.float32)
+	# load
+	n0 = _mm_loadu_ps(data)
+	n1 = _mm_loadu_ps(data + 4)
+	n2 = _mm_loadu_ps(data + 8)
+	n3 = _mm_loadu_ps(data + 12)
+	n4 = _mm_loadu_ps(data + 16)
+	n5 = _mm_loadu_ps(data + 20)
+	n6 = _mm_loadu_ps(data + 24)
+	n7 = _mm_loadu_ps(data + 28)
+	_MM_TRANSPOSE4_PS(n0, n2, n4, n6)
+	_MM_TRANSPOSE4_PS(n1, n3, n5, n7)
+
+	_MM_TRANSPOSE4_PS(v0, v2, v4, v6)
+	_MM_TRANSPOSE4_PS(v1, v3, v5, v7)
+	_mm_storeu_ps(&out[0], v0)
+	_mm_storeu_ps(&out[4], v1)
+	_mm_storeu_ps(&out[8], v2)
+	_mm_storeu_ps(&out[12], v3)
+	_mm_storeu_ps(&out[16], v4)
+	_mm_storeu_ps(&out[20], v5)
+	_mm_storeu_ps(&out[24], v6)
+	_mm_storeu_ps(&out[28], v7)
+	return out
+
+
+cdef uint16_to_fp32(cnp.ndarray[cnp.uint16_t, ndim = 1] a):
+	cdef:
+		unsigned char* data_ptr = <unsigned char *> ...
+		__m128i n0 = _mm_cvtepu16_epi32(_mm_movpi64_epi64(_mm_loadu_si64(<void *> data_ptr)))
+	return n0
+
+def dct_3d_fwd(cnp.ndarray[cnp.uint16_t, ndim = 3] a): # 32x32x32 block
+	cdef:
+		unsigned char i, j, k
+		cnp.ndarray[cnp.uint16_t, ndim = 3] result = np.empty((32, 32, 32), np.float32)
+	for i in prange(32, nogil = True):
+		for j in prange(32, nogil = True):
+			for k in prange(8, nogil = True):
+				result[i, j, k << 2] = uint16_to_fp32(a[i, j, k << 2])
+	for i in prange(32, nogil = True):
+		for j in prange(32, nogil = True):
+			result[i, j] = dct_fwd(result[i, j])
+
+	
