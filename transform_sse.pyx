f4a15d1beaec4eff11d6346a197376ef8bcf702d
Log1cG8z <yyukki2009@gmail.com>
2025-04-04 13:01:05 +0900

    Add files via upload

--- transform_sse.pyx
+++ transform_sse.pyx
@@ -23,23 +23,99 @@ cdef extern from *:
 @cython.nogil
 @cython.boundscheck(False)
 @cython.wraparound(False)
+cdef ndarray_to_c_array(cnp.ndarray[cnp.uint16_t, ndim = 3] a):
+	if not a.flags['C_CONTIGUOUS']: a = np.ascontiguousarray(a)
+	return a
+
+@cython.nogil
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef _mm_lval_ps(float a):
+	cdef __m128 result = _mm_set_ps(a, a, a, a)
+	return result
+
+@cython.nogil
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef _mm_ldarr_ps(cnp.ndarray[cnp.float32_t, ndim = 1] a):
+	cdef:
+		__m128 n0, n1, n2, n3, n4, n5, n6, n7, v0, v1, v2, v3, v4, v5, v6, v7
+		float* data = &a[0]
+	v0 = _mm_loadu_ps(data)
+	v1 = _mm_loadu_ps(data + 4)
+	v2 = _mm_loadu_ps(data + 8)
+	v3 = _mm_loadu_ps(data + 12)
+	v4 = _mm_loadu_ps(data + 16)
+	v5 = _mm_loadu_ps(data + 20)
+	v6 = _mm_loadu_ps(data + 24)
+	v7 = _mm_loadu_ps(data + 28)
+	_MM_TRANSPOSE4_PS(n0, n2, n4, n6)
+	_MM_TRANSPOSE4_PS(n1, n3, n5, n7)
+	n0 = v0
+	n1 = v2
+	n2 = v4
+	n3 = v6
+	n4 = v1
+	n5 = v3
+	n6 = v5
+	n7 = v7
+	return v0, v1, v2, v3, v4, v5, v6, v7
+
+@cython.nogil
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef _mm_starr_ps(__m128 v0, __m128 v1, __m128 v2, __m128 v3, __m128 v4, __m128 v5, __m128 v6, __m128 v7):
+	cdef:
+		__m128 n0, n1, n2, n3, n4, n5, n6, n7
+		cnp.ndarray[cnp.float32_t, ndim = 1] out = np.empty(32, dtype = np.float32)
+	v0 = n0
+	v1 = n4
+	v2 = n1
+	v3 = n5
+	v4 = n2
+	v5 = n6
+	v6 = n3
+	v7 = n7
+	_MM_TRANSPOSE4_PS(n0, n2, n4, n6)
+	_MM_TRANSPOSE4_PS(n1, n3, n5, n7)
+	_mm_storeu_ps(&out[0], n0)
+	_mm_storeu_ps(&out[4], n1)
+	_mm_storeu_ps(&out[8], n2)
+	_mm_storeu_ps(&out[12], n3)
+	_mm_storeu_ps(&out[16], n4)
+	_mm_storeu_ps(&out[20], n5)
+	_mm_storeu_ps(&out[24], n6)
+	_mm_storeu_ps(&out[28], n7)
+	return out
+
+@cython.nogil
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef dct_time_fwd(cnp.ndarray[cnp.float32_t, ndim = 1] a):
 	# dct_time_fwd(np.array(range(32)).astype(np.float32)) = [  28,  -4,  -8,   0, -16,   0,   0,   0,  92,  -4,  -8,   0, -16,   0,   0,   0, 156,  -4,  -8,   0, -16,   0,   0,   0, 220,  -4,  -8,   0, -16,   0,   0,   0]
 	cdef:
 		__m128 n0, n1, n2, n3, n4, n5, n6, n7, v0, v1, v2, v3, v4, v5, v6, v7
-		float* data = &a[0]
 		cnp.ndarray[cnp.float32_t, ndim = 1] out = np.empty(32, dtype = np.float32)
+		float* data = &a[0]
 	# load
-	n0 = _mm_loadu_ps(data)
-	n1 = _mm_loadu_ps(data + 4)
-	n2 = _mm_loadu_ps(data + 8)
-	n3 = _mm_loadu_ps(data + 12)
-	n4 = _mm_loadu_ps(data + 16)
-	n5 = _mm_loadu_ps(data + 20)
-	n6 = _mm_loadu_ps(data + 24)
-	n7 = _mm_loadu_ps(data + 28)
+	v0 = _mm_loadu_ps(data)
+	v1 = _mm_loadu_ps(data + 4)
+	v2 = _mm_loadu_ps(data + 8)
+	v3 = _mm_loadu_ps(data + 12)
+	v4 = _mm_loadu_ps(data + 16)
+	v5 = _mm_loadu_ps(data + 20)
+	v6 = _mm_loadu_ps(data + 24)
+	v7 = _mm_loadu_ps(data + 28)
 	_MM_TRANSPOSE4_PS(n0, n2, n4, n6)
 	_MM_TRANSPOSE4_PS(n1, n3, n5, n7)
+	n0 = v0
+	n1 = v2
+	n2 = v4
+	n3 = v6
+	n4 = v1
+	n5 = v3
+	n6 = v5
+	n7 = v7
 	# stage 1
 	v0 = _mm_add_ps(n0, n4)
 	v1 = _mm_add_ps(n1, n5)
@@ -67,7 +143,15 @@ cdef dct_time_fwd(cnp.ndarray[cnp.float32_t, ndim = 1] a):
 	v5 = _mm_sub_ps(n4, n5)
 	v6 = _mm_add_ps(n6, n7)
 	v7 = _mm_sub_ps(n6, n7)
-	# set to ndarray
+	# store
+	v0 = n0
+	v1 = n4
+	v2 = n1
+	v3 = n5
+	v4 = n2
+	v5 = n6
+	v6 = n3
+	v7 = n7
 	_MM_TRANSPOSE4_PS(v0, v2, v4, v6)
 	_MM_TRANSPOSE4_PS(v1, v3, v5, v7)
 	_mm_storeu_ps(&out[0], v0)
@@ -80,40 +164,108 @@ cdef dct_time_fwd(cnp.ndarray[cnp.float32_t, ndim = 1] a):
 	_mm_storeu_ps(&out[28], v7)
 	return out
 
+@cython.nogil
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef dct_fwd(cnp.ndarray[cnp.float32_t, ndim = 1] a):
 	# dct_fwd(np.array(range(32)).astype(np.float32)) = [28.0, -25.274143, 0.0, -2.2398288, 0.0, -0.44646287, 0.0, -0.039565086, 92.0, -25.274143, 0.0, -2.2398288, 0.0, -0.44646287, 0.0, -0.039565086, 156.0, -25.274143, 0.0, -2.2398288, 0.0, -0.44646287, 0.0, -0.039565086, 220.0, -25.274143, 0.0, -2.2398288, 0.0, -0.44646287, 0.0, -0.039565086]
 	cdef:
-		__m128 n0, n1, n2, n3, n4, n5, n6, n7, a1, a2, a3, a4, a5, v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v17, v18, v19, v20, v23, v24, tmp
+		__m128 n0, n1, n2, n3, n4, n5, n6, n7, v0, v1, v2, v3, v4, v5, v6, v7, a1, a2, a3, a4, a5, tmp
 		float m0, m1, m2, m3, m4
-		float* data = &a[0]
 		cnp.ndarray[cnp.float32_t, ndim = 1] out = np.empty(32, dtype = np.float32)
-	# define constants
-	m0 = 0.7071067811865476
-	m1 = -0.5411961001461969
-	m2 = 1.3065629648763766
-	m3 = 0.38268343236508984
-	m4 = -1
-	a1 = _mm_set_ps(m0, m0, m0, m0)
-	a2 = _mm_set_ps(m1, m1, m1, m1)
-	a3 = _mm_set_ps(m4, m4, m4, m4)
-	a4 = _mm_set_ps(m2, m2, m2, m2)
-	a5 = _mm_set_ps(m3, m3, m3, m3)
+		float* data = &a[0]
 	# load
-	n0 = _mm_loadu_ps(data)
-	n1 = _mm_loadu_ps(data + 4)
-	n2 = _mm_loadu_ps(data + 8)
-	n3 = _mm_loadu_ps(data + 12)
-	n4 = _mm_loadu_ps(data + 16)
-	n5 = _mm_loadu_ps(data + 20)
-	n6 = _mm_loadu_ps(data + 24)
-	n7 = _mm_loadu_ps(data + 28)
+	v0 = _mm_loadu_ps(data)
+	v1 = _mm_loadu_ps(data + 4)
+	v2 = _mm_loadu_ps(data + 8)
+	v3 = _mm_loadu_ps(data + 12)
+	v4 = _mm_loadu_ps(data + 16)
+	v5 = _mm_loadu_ps(data + 20)
+	v6 = _mm_loadu_ps(data + 24)
+	v7 = _mm_loadu_ps(data + 28)
 	_MM_TRANSPOSE4_PS(n0, n2, n4, n6)
 	_MM_TRANSPOSE4_PS(n1, n3, n5, n7)
-	# transform
-	
+	n0 = v0
+	n1 = v2
+	n2 = v4
+	n3 = v6
+	n4 = v1
+	n5 = v3
+	n6 = v5
+	n7 = v7
+	# stage 1
+	v0 = _mm_add_ps(n0, n7)
+	v1 = _mm_add_ps(n1, n6)
+	v2 = _mm_add_ps(n2, n5)
+	v3 = _mm_add_ps(n3, n4)
+	v4 = _mm_sub_ps(n3, n4)
+	v5 = _mm_sub_ps(n2, n5)
+	v6 = _mm_sub_ps(n1, n6)
+	v7 = _mm_sub_ps(n0, n7)
+	# stage 2
+	n0 = _mm_add_ps(v0, v3)
+	n1 = _mm_add_ps(v1, v2)
+	n2 = _mm_sub_ps(v1, v2)
+	n3 = _mm_sub_ps(v0, v3)
+	n4 = _mm_add_ps(v4, v5)
+	n5 = _mm_add_ps(v5, v6)
+	n6 = _mm_add_ps(v6, v7)
+	n7 = v7
+	# stage 3
+	v0 = _mm_add_ps(n0, n1)
+	v1 = _mm_sub_ps(n0, n1)
+	v2 = _mm_add_ps(n2, n3)
+	v3 = n3
+	v4 = n4
+	v5 = n5
+	v6 = n6
+	v7 = n7
+	tmp = _mm_sub_ps(n6, n4)
+	# stage 4
+	n0 = v0
+	n1 = v1
+	n2 = _mm_mul_ps(v2, a1)
+	n3 = v3
+	n4 = _mm_mul_ps(v4, a2)
+	n5 = _mm_mul_ps(v5, a3)
+	n6 = _mm_mul_ps(v6, a4)
+	tmp = _mm_mul_ps(tmp, a5)
+	# stage 5
+	v0 = n0
+	v1 = n1
+	v2 = _mm_add_ps(n3, n2)
+	v3 = _mm_sub_ps(n3, n2)
+	v4 = _mm_sub_ps(n4, tmp)
+	v5 = _mm_add_ps(n7, n5)
+	v6 = _mm_sub_ps(n6, tmp)
+	v7 = _mm_sub_ps(n7, n5)
+	# stage 6
+	n0 = v0
+	n1 = v1
+	n2 = v2
+	n3 = v3
+	n4 = _mm_add_ps(v7, v4)
+	n5 = _mm_add_ps(v5, v6)
+	n6 = _mm_sub_ps(v5, v6)
+	n7= _mm_sub_ps(v7, v4)
+	# stage 7
+	v0 = n0
+	v4 = n1
+	v2 = n2
+	v6 = n3
+	v5 = n4
+	v1 = n5
+	v7 = n6
+	v3 = n7
 	# store
+	n0 = v0
+	n1 = v4
+	n2 = v1
+	n3 = v5
+	n4 = v2
+	n5 = v6
+	n6 = v3
+	n7 = v7
 	_MM_TRANSPOSE4_PS(n0, n2, n4, n6)
 	_MM_TRANSPOSE4_PS(n1, n3, n5, n7)
 	_mm_storeu_ps(&out[0], n0)
@@ -129,7 +281,7 @@ cpdef dct_fwd(cnp.ndarray[cnp.float32_t, ndim = 1] a):
 @cython.nogil
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef uint16_to_fp32(cnp.ndarray[cnp.uint16_t, ndim = 1] a):
+cpdef uint16_to_fp32(cnp.ndarray[cnp.uint16_t, ndim = 1] a):
 	cdef:
 		__m128i n0, n1
 		cnp.uint16_t* data_ptr = &a[0]
@@ -137,10 +289,25 @@ cdef uint16_to_fp32(cnp.ndarray[cnp.uint16_t, ndim = 1] a):
 	n0 = _mm_cvtepu16_epi32(n1)
 	return n0
 
+@cython.nogil
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef dct_3d_fwd(cnp.ndarray[cnp.uint16_t, ndim = 3] a): # 32x32x32 block
+cpdef dct_3d_fwd(short[:, :, :] arr): # 32x32x32 block
 	cdef:
-		unsigned char i, j, k
 		cnp.ndarray[cnp.uint16_t, ndim = 3] result = np.empty((32, 32, 32), np.float32)
-	
+		float[:, :, :] tmp = result
+		unsigned char h, i, j, k, l, m
+	for i in prange(32, nogil = True):
+		for j in prange(32, nogil = True):
+			for k in prange(8, nogil = True):
+				l = k << 2
+				m = l + 4
+				tmp[i, j, l:m] = uint16_to_fp32(np.asarray(arr[i, j, l:m]))
+			tmp[:, i, j] = dct_time_fwd(tmp[:, i, j])
+	for i in prange(32, nogil = True):
+		for j in prange(32, nogil = True):
+			tmp[j, :, i] = dct_fwd(tmp[j, :, i])
+	for i in prange(32, nogil = True):
+		for j in prange(32, nogil = True):
+			tmp[i, j, :] = dct_fwd(tmp[i, j, :])
+	return np.asarray(tmp)
