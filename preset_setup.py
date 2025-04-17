b3c164941dca368a284ab2de0ee45ca4f56e81d3
Log1cG8z <yyukki2009@gmail.com>
2025-04-17 20:04:15 +0900

    インライン展開を積極的に
    まあ、全関数インライン指定してるのであまり意味はないけど

--- preset_setup.py
+++ preset_setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-package = Extension('test', ['test.pyx'], include_dirs=[numpy.get_include()], extra_compile_args=['/O2', '/Ob2', '/Oi', '/Ot', '/GL', '/fp:fast', '/arch:SSE4.2'], extra_link_args=['/LTCG'])
+package = Extension('test', ['test.pyx'], include_dirs=[numpy.get_include()], extra_compile_args=['/O2', '/Ob3', '/Oi', '/Ot', '/GL', '/fp:fast', '/arch:SSE4.2'], extra_link_args=['/LTCG'])
 setup(ext_modules=cythonize([package]))
