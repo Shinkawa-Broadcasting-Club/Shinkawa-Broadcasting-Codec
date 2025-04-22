21c87991a9e96cd2caa230ac3bdd6ff930b16892
Log1cG8z <yyukki2009@gmail.com>
2025-04-23 06:13:05 +0900

    Add files via upload

--- vs_arr.pyx
+++ vs_arr.pyx
@@ -4,15 +4,18 @@ cimport numpy as cnp
 cdef inline VStoNP(i):
 	cdef:
 		cnp.ndarray[cnp.uint16_t, ndim = 4] arr = np.empty((3, i.num_frames, i.height, i.width), np.uint16)
-		cnp.ndarray[cnp.uint16_t, ndim = 2] t = np.empty((i.height, i.width), np.uint16)
+		cnp.ndarray[cnp.uint16_t, ndim = 2] tmp = np.empty((i.height, i.width), np.uint16)
+		unsigned short[:, :] t = tmp
 		unsigned short[:, :, :] y = arr[0]
 		unsigned short[:, :, :] u = arr[1]
 		unsigned short[:, :, :] v = arr[2]
 		int m
 	for m in range(i.num_frames):
-		y[m] = np.array(i.get_frame(m)[0], copy=False)
-		u[m] = np.array(i.get_frame(m)[1], copy=False)
-		v[m] = np.array(i.get_frame(m)[2], copy=False)
+		for n in range(3):
+			tmp = np.array(i.get_frame(m)[n], copy=False)
+			if   n == 0: y[m] = t
+			elif n == 1: u[m] = t
+			else:        v[m] = t
 	return y, u, v
 
 def vs_to_np(i): return VStoNP(i)
