ee43ac227f973c032fe3936571ebc6c2a044e26e
Log1cG8z <yyukki2009@gmail.com>
2025-05-02 06:17:03 +0900

    Add files via upload

--- corr_coef.pyx
+++ corr_coef.pyx
@@ -2,27 +2,14 @@
 from libc.stdlib cimport rand, srand, malloc, free
 from libc.string cimport memcpy
 from libc.time cimport time
-from cython.parallel import parallel, prange
-cdef extern from "nmmintrin.h" nogil:
-    ctypedef struct __m128: pass
-    __m128 _mm_load_ss (const float* mem_addr)
-    __m128 _mm_loadu_ps (const float* mem_addr)
-    __m128 _mm_rsqrt_ss (__m128 a)
-    __m128 _mm_add_ps (__m128 a, __m128 b)
-    __m128 _mm_hadd_ps (__m128 a, __m128 b)
-    void _mm_store_ss (float* mem_addr, __m128 a)
-    void _mm_storeu_ps (float* mem_addr, __m128 a)
+from cython.parallel import parallel, prange, reduction
+cdef extern from "intrin.h" nogil:
+    unsigned char _BitScanReverse(unsigned long *Index, unsigned long Mask)
 
-cdef inline void mul256_flt(float f, float n) nogil:
-    cdef int d
-    memcpy(&d, &f, sizeof(f))
-    d += 0x04000000
-    memcpy(&n, &d, sizeof(d))
-
-cdef inline rand256(float[:, :] arr):
+cdef inline rand256(int[:, :] arr) nogil:
     cdef:
         int N = arr.shape[1]
-        float[3][256] out
+        int[3][256] out
         int* indices = <int*> malloc(N * sizeof(int))
         int i, j, k, tmp
     if N < 256: raise ValueError("入力配列の第2軸（列数）は256以上である必要があります。")
@@ -39,45 +26,73 @@ cdef inline rand256(float[:, :] arr):
     free(indices)
     return out
 
-cdef inline fsum(float[256] arr):
-    cdef:
-        __m128[64] tmp
-        float[4] dst
-        int i
-    with nogil, parallel():
-        for i in prange(64):       tmp[i] = _mm_loadu_ps(&arr[i << 2])
-        for i in prange(0, 32, 2): tmp[i] = _mm_add_ps(tmp[i], tmp[i + 1])
-        for i in prange(0, 16, 4): tmp[i] = _mm_add_ps(tmp[i], tmp[i + 2])
-        for i in prange(0, 8, 8):  tmp[i] = _mm_add_ps(tmp[i], tmp[i + 4])
-        for i in prange(0, 4, 16): tmp[i] = _mm_add_ps(tmp[i], tmp[i + 8])
-        for i in prange(0, 2, 32): tmp[i] = _mm_add_ps(tmp[i], tmp[i + 16])
-    tmp[0] = _mm_add_ps(tmp[0], tmp[32])
-    for i in range(2): tmp[0] = _mm_hadd_ps(tmp[0], tmp[0])
-    _mm_storeu_ps(&dst[0], tmp[0])
-    return dst[0]
+cdef inline int ilog2(unsigned int v) nogil:
+    """
+    v の最上位に立っているビットのインデックスを返します。
+    例: 32 (0b00100000) の場合、返り値は 5 (0-indexed) になります。
+    入力が 0 の場合は ValueError を発生させます。
+    """
+    cdef unsigned long index
+    if _BitScanReverse(&index, v) == 0: raise ValueError("v must be non-zero")
+    return index
 
-cdef inline rsqrt(float n):
-    cdef float d
-    _mm_store_ss(&d, _mm_rsqrt_ss(_mm_load_ss(&n)))
-    return d
-
-cdef inline corr_coef(int[:, :] arr):
+cdef inline corr_coef(int[:, :] arr) nogil:
     cdef:
-        int[2][256] sample = rand256(arr)
-        float[5][256] tmp
-        float x, y, xx, yy, xy
+        int[3][256] sample = rand256(arr)
+        int x = sample[0]
+        int y = sample[1]
+        int z = sample[2]
         unsigned short i, j
     with nogil, parallel():
-        for i in prange(5):
-            for j in prange(256):
-                if i == 0: tmp[0][j] = sample[0][j]
-                if i == 1: tmp[1][j] = sample[1][j]
-                if i == 2: tmp[2][j] = sample[0][j] ** 2
-                if i == 3: tmp[3][j] = sample[1][j] ** 2
-                if i == 4: tmp[4][j] = sample[0][j] * sample[1][j]
-    x = fsum(tmp[0])
-    y = fsum(tmp[1])
-    mul256_flt(fsum(tmp[2]), xx)
-    mul256_flt(fsum(tmp[3]), yy)
-    mul256_flt(fsum(tmp[4]), xy)
-    return (xy - x * y) * rsqrt((xx - x ** 2) * (yy - y ** 2))
+        cdef int n = 256
+        cdef int i
+        cdef int sx = 0, sy = 0, sz = 0
+
+        # 各配列の合計値を並列化して計算（reductionを利用）
+        for i in prange(n):
+            sx += x[i]
+            sy += y[i]
+            sz += z[i]
+
+    # 256で割る＝右に8ビットシフト
+    cdef int mean_x = sx >> 8
+    cdef int mean_y = sy >> 8
+    cdef int mean_z = sz >> 8
+
+    cdef int Sxx = 0, Syy = 0, Szz = 0
+    cdef int Sxy = 0, Sxz = 0, Syz = 0
+    cdef int dx, dy, dz
+
+    # 分散（Sxx,Syy,Szz）および共分散（Sxy,Sxz,Syz）の計算
+    for i in prange(n):
+        dx = x[i] - mean_x
+        dy = y[i] - mean_y
+        dz = z[i] - mean_z
+        Sxx += dx ** 2
+        Syy += dy ** 2
+        Szz += dz ** 2
+        Sxy += dx * dy
+        Sxz += dx * dz
+        Syz += dy * dz
+
+    # 平均で割るのは右シフト8で
+    cdef int cov_xx = Sxx >> 8
+    cdef int cov_yy = Syy >> 8
+    cdef int cov_zz = Szz >> 8
+    cdef int cov_xy = Sxy >> 8
+    cdef int cov_xz = Sxz >> 8
+    cdef int cov_yz = Syz >> 8
+
+    # 共分散行列の行列式を計算
+    cdef int det = cov_xx * cov_yy * cov_zz - cov_xx * cov_yz ** 2 - cov_xy * cov_xy * cov_zz - cov_xz ** 2 * cov_yy + 2 * cov_xy * cov_yz * cov_xz
+
+    # 各変量の分散の積
+    cdef int prod = cov_xx * cov_yy * cov_zz
+
+    # もし det または prod が正でなければ、計算不能と見なす
+    if det <= 0 or prod <= 0: return 0
+
+    # 対数を整数近似 (log₂の整数部分) を用いて相互情報量を計算
+    cdef int log_prod = ilog2(prod)
+    cdef int log_det = ilog2(det)
+    return (log_prod - log_det) >> 1 # 0.5倍するため右シフト1（1/2倍）
