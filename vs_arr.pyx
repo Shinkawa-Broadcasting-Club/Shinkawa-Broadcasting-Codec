d0fb942f7da6279626ed4bfbab9feaf0bd0e3b0c
Log1cG8z <yyukki2009@gmail.com>
2025-05-04 13:08:24 +0900

    Update vs_arr.pyx

--- vs_arr.pyx
+++ vs_arr.pyx
@@ -1,9 +1,7 @@
 import numpy as np
 cimport numpy as cnp
-import vapoursynth as vs
-import joblib
 
-cdef get_vsframe(int m, int n, vs.VideoNode i): return np.array(i.get_frame(m)[n], copy=False)
+cdef get_vsframe(int m, int n, i): return np.array(i.get_frame(m)[n], copy=False)
 cdef vs_to_np(i):
 	cdef:
 		cnp.ndarray[cnp.uint16_t, ndim = 3] y = np.empty((i.num_frames, i.height, i.width))
@@ -13,5 +11,7 @@ cdef vs_to_np(i):
 	for m in range(i.num_frames):
 		for n in range(i.format.num_planes):
 			t = get_vsframe(m, n, i)
-			if n == 0:
+			if n == 0: y[m] = t
+			if n == 1: u[m] = t
+			if n == 2: v[m] = t
 	return y, u, v
