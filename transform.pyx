8d6216f202acecd28baafc53d97df6b85e9ce3fd
Log1cG8z <yyukki2009@gmail.com>
2025-05-11 16:57:54 +0900

    DCT(SSE)実装

---
+++ transform.pyx
@@ -0,0 +1,61 @@
+# cython: language_level=3, boundscheck=False, wraparound=False, cdivision=True, initializedcheck=False, nonecheck=False
+from cython.parallel import prange, parallel
+cimport numpy as cnp
+import numpy as np
+cdef extern from "<immintrin.h>" nogil:
+    ctypedef struct __m128i: pass
+    __m128i _mm_add_epi32(__m128i a, __m128i b)
+    __m128i _mm_sub_epi32(__m128i a, __m128i b)
+    __m128i _mm_hadd_epi32(__m128i a, __m128i b)
+    __m128i _mm_hsub_epi32(__m128i a, __m128i b)
+    __m128i _mm_sra_epi32(__m128i a, __m128i count)
+
+cdef inline void dct_fwd_sse(__m128i[128] p, int s, int o) nogil:
+    cdef __m128i[8] a, b
+    # stage 1
+    a[0] = _mm_add_epi32(p[(0 << s) + o], p[(7 << s) + o])
+    a[1] = _mm_add_epi32(p[(1 << s) + o], p[(6 << s) + o])
+    a[2] = _mm_add_epi32(p[(2 << s) + o], p[(5 << s) + o])
+    a[3] = _mm_add_epi32(p[(3 << s) + o], p[(4 << s) + o])
+    a[4] = _mm_sub_epi32(p[(0 << s) + o], p[(7 << s) + o])
+    a[5] = _mm_sub_epi32(p[(1 << s) + o], p[(6 << s) + o])
+    a[6] = _mm_sub_epi32(p[(2 << s) + o], p[(5 << s) + o])
+    a[7] = _mm_sub_epi32(p[(3 << s) + o], p[(4 << s) + o])
+    # stage 2
+    b[0] = _mm_add_epi32(a[0], a[3])
+    b[1] = _mm_add_epi32(a[1], a[2])
+    b[2] = _mm_sub_epi32(a[0], a[3])
+    b[3] = _mm_sub_epi32(a[1], a[2])
+    b[4] = _mm_add_epi32(_mm_add_epi32(a[5], a[6]), _mm_add_epi32(_mm_sra_epi32(a[4], 1), a[4]))
+    b[5] = _mm_sub_epi32(_mm_sub_epi32(a[4], a[7]), _mm_add_epi32(_mm_sra_epi32(a[6], 1), a[6]))
+    b[6] = _mm_sub_epi32(_mm_add_epi32(a[4], a[7]), _mm_add_epi32(_mm_sra_epi32(a[5], 1), a[5]))
+    b[7] = _mm_add_epi32(_mm_sub_epi32(a[5], a[6]), _mm_add_epi32(_mm_sra_epi32(a[7], 1), a[7]))
+    # stage 3
+    p[(0 << s) + o] = _mm_add_epi32(b[0], b[1])
+    p[(1 << s) + o] = _mm_add_epi32(b[4], _mm_sra_epi32(b[7], 2))
+    p[(2 << s) + o] = _mm_add_epi32(_mm_sra_epi32(b[3], 1), b[2])
+    p[(3 << s) + o] = _mm_add_epi32(_mm_sra_epi32(b[6], 2), b[5])
+    p[(4 << s) + o] = _mm_sub_epi32(b[0], b[1])
+    p[(5 << s) + o] = _mm_sub_epi32(b[6], _mm_sra_epi32(b[5], 2))
+    p[(6 << s) + o] = _mm_sub_epi32(_mm_sra_epi32(b[2], 1), b[3])
+    p[(7 << s) + o] = _mm_sub_epi32(_mm_sra_epi32(b[4], 2), b[7])
+
+cdef inline void dct_time_sse(__m128i[128] p, int o) nogil:
+    cdef __m128i[2] a, b
+    # stage 1
+    a[0] = _mm_add_epi32(p[o << 1], p[(o << 1) + 1])
+    a[1] = _mm_sub_epi32(p[o << 1], p[(o << 1) + 1])
+    # stage 2
+    b[0] = _mm_hadd_epi32(a[0], a[1])
+    b[1] = _mm_hsub_epi32(a[0], a[1])
+    # stage 3
+    p[o << 1] = _mm_hadd_epi32(b[0], b[1])
+    p[(o << 1) + 1] = _mm_hsub_epi32(b[0], b[1])
+
+cdef inline void dct3d_sse(__m128i[128] i) nogil:
+    cdef int j, k
+    for j in range(32):
+        for k in range(2): dct_fwd_sse(i, 2, (j << 2) + k)
+    for j in range()
+
+cdef inline void idct3d_sse(__m128i[128] i) nogil
