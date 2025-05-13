d0811d4f3d1d03f936577d4572d443b19fdd8f7b
Log1cG8z <yyukki2009@gmail.com>
2025-05-13 17:18:29 +0900

    3次元ポインタ追加
    いずれかはlibav系で読み込みたい

---
+++ array.pyx
@@ -0,0 +1,82 @@
+# cython: language_level=3, boundscheck=False, wraparound=False, cdivision=True, initializedcheck=False, nonecheck=False
+from cython.parallel import prange
+from libc.stdlib cimport malloc, free
+from cython cimport sizeof
+
+cdef inline int*** alloc_3d_int_arr(int x, int y, int z) nogil:
+    cdef int*** arr = <int***> malloc(x * sizeof(int**))
+    if arr == NULL: raise MemoryError("配列のメモリ確保に失敗(次元:1)")
+    cdef int i, j, k, l
+    for i in prange(x):
+        arr[i] = <int**> malloc(y * sizeof(int*))
+        if arr[i] == NULL:
+            for j in prange(i): free(arr[j])
+            raise MemoryError("配列のメモリ確保に失敗(次元:2)")
+        for j in prange(y):
+            arr[i][j] = <int*> malloc(z * sizeof(int))
+            if arr[i][j] == NULL:
+                for k in prange(j): free(arr[i][k])
+                for k in prange(i):
+                    for l in prange(y):
+                        if arr[k][l] != NULL: free(arr[k][l])
+                    free(arr[k])
+                free(arr)
+                raise MemoryError("配列のメモリ確保に失敗(次元:3)")
+    return arr
+
+cdef inline unsigned short*** alloc_3d_ushort_arr(int x, int y, int z) nogil:
+    cdef unsigned short*** arr = <unsigned short***> malloc(x * sizeof(unsigned short**))
+    if arr == NULL: raise MemoryError("配列のメモリ確保に失敗(次元:1)")
+    cdef int i, j, k, l
+    for i in prange(x):
+        arr[i] = <unsigned short**> malloc(y * sizeof(unsigned short*))
+        if arr[i] == NULL:
+            for j in prange(i): free(arr[j])
+            raise MemoryError("配列のメモリ確保に失敗(次元:2)")
+        for j in prange(y):
+            arr[i][j] = <unsigned short*> malloc(z * sizeof(unsigned short))
+            if arr[i][j] == NULL:
+                for k in prange(j): free(arr[i][k])
+                for k in prange(i):
+                    for l in prange(y):
+                        if arr[k][l] != NULL: free(arr[k][l])
+                    free(arr[k])
+                free(arr)
+                raise MemoryError("配列のメモリ確保に失敗(次元:3)")
+    return arr
+
+cdef inline unsigned char*** alloc_3d_ushort_arr(int x, int y, int z) nogil:
+    cdef unsigned char*** arr = <unsigned char***> malloc(x * sizeof(unsigned char**))
+    if arr == NULL: raise MemoryError("配列のメモリ確保に失敗(次元:1)")
+    cdef int i, j, k, l
+    for i in prange(x):
+        arr[i] = <unsigned char**> malloc(y * sizeof(unsigned char*))
+        if arr[i] == NULL:
+            for j in prange(i): free(arr[j])
+            raise MemoryError("配列のメモリ確保に失敗(次元:2)")
+        for j in prange(y):
+            arr[i][j] = <unsigned char*> malloc(z * sizeof(unsigned char))
+            if arr[i][j] == NULL:
+                for k in prange(j): free(arr[i][k])
+                for k in prange(i):
+                    for l in prange(y):
+                        if arr[k][l] != NULL: free(arr[k][l])
+                    free(arr[k])
+                free(arr)
+                raise MemoryError("配列のメモリ確保に失敗(次元:3)")
+    return arr
+
+cdef inline void free_3d_int_arr(int*** arr) nogil:
+    cdef int i, j, k, l
+    free(arr)
+    if arr != NULL: raise MemoryError("配列のメモリ開放に失敗")
+
+cdef inline void free_3d_ushort_arr(unsigned short*** arr) nogil:
+    cdef int i, j, k, l
+    free(arr)
+    if arr != NULL: raise MemoryError("配列のメモリ開放に失敗")
+
+cdef inline void free_3d_ushort_arr(unsigned char*** arr) nogil:
+    cdef int i, j, k, l
+    free(arr)
+    if arr != NULL: raise MemoryError("配列のメモリ開放に失敗")
--- transform.pyx
+++ transform.pyx
@@ -13,11 +13,11 @@ cdef extern from "<immintrin.h>" nogil:
 cdef inline void dct_fwd_sse(__m128i[128] p, int s, int o) nogil:
     cdef __m128i[8] a, b
     # stage 1
-    a[0] = _mm_add_epi32(p[(0 << s) + o], p[(7 << s) + o])
+    a[0] = _mm_add_epi32(p[o], p[(7 << s) + o])
     a[1] = _mm_add_epi32(p[(1 << s) + o], p[(6 << s) + o])
     a[2] = _mm_add_epi32(p[(2 << s) + o], p[(5 << s) + o])
     a[3] = _mm_add_epi32(p[(3 << s) + o], p[(4 << s) + o])
-    a[4] = _mm_sub_epi32(p[(0 << s) + o], p[(7 << s) + o])
+    a[4] = _mm_sub_epi32(p[o], p[(7 << s) + o])
     a[5] = _mm_sub_epi32(p[(1 << s) + o], p[(6 << s) + o])
     a[6] = _mm_sub_epi32(p[(2 << s) + o], p[(5 << s) + o])
     a[7] = _mm_sub_epi32(p[(3 << s) + o], p[(4 << s) + o])
@@ -31,7 +31,7 @@ cdef inline void dct_fwd_sse(__m128i[128] p, int s, int o) nogil:
     b[6] = _mm_sub_epi32(_mm_add_epi32(a[4], a[7]), _mm_add_epi32(_mm_sra_epi32(a[5], 1), a[5]))
     b[7] = _mm_add_epi32(_mm_sub_epi32(a[5], a[6]), _mm_add_epi32(_mm_sra_epi32(a[7], 1), a[7]))
     # stage 3
-    p[(0 << s) + o] = _mm_add_epi32(b[0], b[1])
+    p[o] = _mm_add_epi32(b[0], b[1])
     p[(1 << s) + o] = _mm_add_epi32(b[4], _mm_sra_epi32(b[7], 2))
     p[(2 << s) + o] = _mm_add_epi32(_mm_sra_epi32(b[3], 1), b[2])
     p[(3 << s) + o] = _mm_add_epi32(_mm_sra_epi32(b[6], 2), b[5])
@@ -39,6 +39,36 @@ cdef inline void dct_fwd_sse(__m128i[128] p, int s, int o) nogil:
     p[(5 << s) + o] = _mm_sub_epi32(b[6], _mm_sra_epi32(b[5], 2))
     p[(6 << s) + o] = _mm_sub_epi32(_mm_sra_epi32(b[2], 1), b[3])
     p[(7 << s) + o] = _mm_sub_epi32(_mm_sra_epi32(b[4], 2), b[7])
+
+cdef inline void dct_bwd_sse(__m128i[128] p, int s, int o) nogil:
+    cdef __m128i[8] a, b
+    # stage 1
+    a[0] = _mm_add_epi32(p[o], p[(4 << s) + o])
+    a[1] = _mm_sub_epi32(p[o], p[(4 << s) + o])
+    a[2] = _mm_sub_epi32(p[(6 << s) + o], _mm_sra_epi32(p[(2 << s) + o], 1))
+    a[3] = _mm_add_epi32(p[(2 << s) + o], _mm_sra_epi32(p[(6 << s) + o], 1))
+    a[4] = _mm_sub_epi32(_mm_sub_epi32(p[(5 << s) + o], p[(3 << s) + o]), _mm_add_epi32(p[(7 << s) + o], _mm_sra_epi32(p[(7 << s) + o], 1)))
+    a[5] = _mm_sub_epi32(_mm_add_epi32(p[(7 << s) + o], p[(1 << s) + o]), _mm_add_epi32(p[(3 << s) + o], _mm_sra_epi32(p[(3 << s) + o], 1)))
+    a[6] = _mm_add_epi32(_mm_sub_epi32(p[(7 << s) + o], p[(1 << s) + o]), _mm_add_epi32(p[(5 << s) + o], _mm_sra_epi32(p[(5 << s) + o], 1)))
+    a[7] = _mm_add_epi32(_mm_add_epi32(p[(5 << s) + o], p[(3 << s) + o]), _mm_add_epi32(p[(1 << s) + o], _mm_sra_epi32(p[(1 << s) + o], 1)))
+    # stage 2
+    b[0] = _mm_add_epi32(a[0], a[3])
+    b[1] = _mm_add_epi32(a[4], _mm_sra_epi32(a[7], 2))
+    b[2] = _mm_sub_epi32(a[1], a[2])
+    b[3] = _mm_add_epi32(a[5], _mm_sra_epi32(a[6], 2))
+    b[4] = _mm_add_epi32(a[1], a[2])
+    b[5] = _mm_sub_epi32(a[6], _mm_sra_epi32(a[5], 2))
+    b[6] = _mm_sub_epi32(a[0], a[3])
+    b[7] = _mm_sub_epi32(a[7], _mm_sra_epi32(a[4], 2))
+    # stage 3
+    p[o] = _mm_add_epi32(b[0], b[7])
+    p[(1 << s) + o] = _mm_sub_epi32(b[2], b[5])
+    p[(2 << s) + o] = _mm_add_epi32(b[4], b[3])
+    p[(3 << s) + o] = _mm_add_epi32(b[6], b[1])
+    p[(4 << s) + o] = _mm_sub_epi32(b[6], b[1])
+    p[(5 << s) + o] = _mm_sub_epi32(b[4], b[3])
+    p[(6 << s) + o] = _mm_add_epi32(b[2], b[5])
+    p[(7 << s) + o] = _mm_sub_epi32(b[0], b[7])
 
 cdef inline void dct_time_sse(__m128i[128] p, int o) nogil:
     cdef __m128i[2] a, b
@@ -55,7 +85,15 @@ cdef inline void dct_time_sse(__m128i[128] p, int o) nogil:
 cdef inline void dct3d_sse(__m128i[128] i) nogil:
     cdef int j, k
     for j in range(32):
-        for k in range(2): dct_fwd_sse(i, 2, (j << 2) + k)
-    for j in range()
+        for k in range(2): dct_fwd_sse(i, 1, (j << 1) + k)
+    for j in range(4):
+        for k in range(16): dct_fwd_sse(i, 4, (j << 4) + k)
+    for j in range(64): dct_fwd_sse(i, j << 1)
 
-cdef inline void idct3d_sse(__m128i[128] i) nogil
+cdef inline void idct3d_sse(__m128i[128] i) nogil:
+    cdef int j, k
+    for j in range(64): dct_fwd_sse(i, j << 1)
+    for j in range(4):
+        for k in range(16): dct_bwd_sse(i, 4, (j << 4) + k)
+    for j in range(32):
+        for k in range(2): dct_bwd_sse(i, 1, (j << 1) + k)
